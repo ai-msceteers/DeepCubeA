5f3afa4c9d9b6304391e7f0bdc3e21180819a0a5
Basil L. Contovounesios <contovob@tcd.ie>
2021-04-23 23:35:56 +0100

    Make A* script more verbose

--- scripts/astar.sh
+++ scripts/astar.sh
@@ -2,14 +2,18 @@
 
 for env in cube2 cube3; do
   for weight in 0.0 0.1 0.2 0.3 0.4 0.5 0.6; do
-    python3 search_methods/astar.py \
-            --states "data/${env}/back100/data_0.pkl" \
-            --model "saved_models/${env}/current/" \
-            --env "${env}" \
-            --weight "${weight}" \
-            --batch_size 100 \
-            --results_dir "results/${env}weight${weight}/" \
-            --language python \
+    states="data/${env}/back100/data_0.pkl"
+    model="saved_models/${env}/current/"
+    results="results/${env}weight${weight}/"
+    echo "states ${states} model ${model} results ${results}"
+    python3 search_methods/astar.py        \
+            --states          "${states}"  \
+            --model           "${model}"   \
+            --env             "${env}"     \
+            --weight          "${weight}"  \
+            --batch_size      100          \
+            --results_dir     "${results}" \
+            --language        python       \
             --nnet_batch_size 100
   done
 done
